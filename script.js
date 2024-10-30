4f9ab4466f287a0e7c229ac16aca59da18a7caf5
quyen26112 <quyentm22@uef.edu.vn>
2024-10-31 00:18:18 +0700

    fix bug

--- script.js
+++ script.js
@@ -1,8 +1,63 @@
-// JavaScript cho tương tác nút
-document.querySelector('.btn--yes').addEventListener('click', () => {
-  alert('Anh biết mà, bé iu tha lỗi cho anh rồi 🥰');
+"use strict";
+
+const titleElement = document.querySelector(".title");
+const buttonsContainer = document.querySelector(".buttons");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
+const catImg = document.querySelector(".cat-img");
+
+const MAX_IMAGES = 5;
+
+let play = true;
+let noCount = 0;
+
+yesButton.addEventListener("click", handleYesClick);
+
+noButton.addEventListener("click", function () {
+  if (play) {
+    noCount++;
+    const imageIndex = Math.min(noCount, MAX_IMAGES);
+    changeImage(imageIndex);
+    resizeYesButton();
+    updateNoButtonText();
+    if (noCount === MAX_IMAGES) {
+      play = false;
+    }
+  }
 });
 
-document.querySelector('.btn--no').addEventListener('click', () => {
-  alert('Không bao giờ ư? 🥺 Anh sẽ chờ cho đến khi bé đổi ý...');
-});
+function handleYesClick() {
+  titleElement.innerHTML = "Hứa không làm m buồn nữa đâu mà :3";
+  buttonsContainer.classList.add("hidden");
+  changeImage("yes");
+}
+
+function resizeYesButton() {
+  const computedStyle = window.getComputedStyle(yesButton);
+  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+  const newFontSize = fontSize * 1.6;
+
+  yesButton.style.fontSize = `${newFontSize}px`;
+}
+
+function generateMessage(noCount) {
+  const messages = [
+    "Không Bao Giờ",
+    "T bicc lỗi rồi mà",
+    "Mong m tha lỗi choo t :((",
+    "T saii rồi , t đáng trách ạ",
+    "M đừng giận t nữa nhoo",
+    "T xin lũi m nhìu nà",
+  ];
+
+  const messageIndex = Math.min(noCount, messages.length - 1);
+  return messages[messageIndex];
+}
+
+function changeImage(image) {
+  catImg.src = `img/cat-${image}.jpg`;
+}
+
+function updateNoButtonText() {
+  noButton.innerHTML = generateMessage(noCount);
+}
