278d1b3d041b62560bc97480381f0a29982ec8c0
quyen26112 <quyentm22@uef.edu.vn>
2024-10-31 00:13:28 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript cho tương tác nút
+document.querySelector('.btn--yes').addEventListener('click', () => {
+  alert('Anh biết mà, bé iu tha lỗi cho anh rồi 🥰');
+});
+
+document.querySelector('.btn--no').addEventListener('click', () => {
+  alert('Không bao giờ ư? 🥺 Anh sẽ chờ cho đến khi bé đổi ý...');
+});
